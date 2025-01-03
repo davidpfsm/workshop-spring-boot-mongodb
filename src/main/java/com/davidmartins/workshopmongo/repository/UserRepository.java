65b1571a5b24794eb86d086c721b1add3bcf133b
David Martins <davidpfsmartins@gmail.com>
2025-01-02 22:32:18 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/davidmartins/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.davidmartins.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.davidmartins.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
